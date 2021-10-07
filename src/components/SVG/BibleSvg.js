aeaca50139f3b65aad8d39c7efce836f2e786465
lmk-kevin <kevin@lmksystem.fr>
2021-10-07 10:47:44 +0200

    Fix

--- src/components/SVG/BibleSvg.js
+++ src/components/SVG/BibleSvg.js
@@ -1,19 +1,11 @@
-import React from "react";
-import { SvgXml } from "react-native-svg";
+import React from 'react'
+import { SvgXml } from 'react-native-svg'
 
-const BibleSvg = ({ type, color }) => {
-  if (type) {
-    if (type == "arrowdown") {
-        var xml = `<svg xmlns="http://www.w3.org/2000/svg" width="15.324" height="9.443" viewBox="0 0 15.324 9.443">
+const xml = `<svg xmlns="http://www.w3.org/2000/svg" width="15.324" height="9.443" viewBox="0 0 15.324 9.443">
         <g id="Composant_87_1" data-name="Composant 87 – 1" transform="translate(0.731 0.696)">
-          <path id="Tracé_377" data-name="Tracé 377" d="M256.36,254.318l6.81,7.3,7.065-7.3" transform="translate(-256.36 -254.318)" fill="none" stroke=`+(color ? color : 'black')+` stroke-width="2"/>
+          <path id="Tracé_377" data-name="Tracé 377" d="M256.36,254.318l6.81,7.3,7.065-7.3" transform="translate(-256.36 -254.318)" fill="none" stroke="black" stroke-width="2"/>
         </g>
       </svg>
-      `;
-      }
-      return <SvgXml xml={xml} width="100%" height="100%" />;
-  }else{
-      return null
-  }
-}
-export default BibleSvg;
+      `
+
+export default () => <SvgXml xml={xml} width="100%" height="100%" />
